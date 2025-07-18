dca17cc59ecdbccb5a3a08089e08907312aa034b
YOURNAME <Maramabdulaziz1>
2025-07-18 06:04:53 +0300

    Initial project setup

---
+++ src/main/java/com/team10/smarthospital/SmarthospitalApplication.java
@@ -0,0 +1,13 @@
+package com.team10.smarthospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarthospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarthospitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team10/smarthospital/SmarthospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team10.smarthospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarthospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
