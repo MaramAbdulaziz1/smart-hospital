f1527967cb5e49b6790b18fac3e9092af87bb85b
YOURNAME <Maramabdulaziz1>
2025-07-25 04:31:10 +0300

    Frontend: Added homepage with login pop-up, registration page, and linked pages with buttons

--- src/main/java/com/team10/smarthospital/controllers/RegisterController.java
+++ src/main/java/com/team10/smarthospital/controllers/RegisterController.java
@@ -4,11 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class RegisterController {
 
-    @GetMapping("/")
-    public String home() {
-        return "homePage"; // to load homePage.html from templates folder
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register";  // loads register.html
     }
 }
-
