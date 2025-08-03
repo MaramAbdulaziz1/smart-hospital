b907e23b4e25543764c99a54ad2f09bbfe1450b7
Liam Chen <tychen5555@gmail.com>
2025-08-04 00:41:48 +0100

    feat/Email Verify page

--- src/main/java/com/team10/smarthospital/controllers/RegisterController.java
+++ src/main/java/com/team10/smarthospital/controllers/RegisterController.java
@@ -10,4 +10,10 @@ public class RegisterController {
     public String registerPage() {
         return "register";  // loads register.html
     }
+
+    @GetMapping("/EmailVerify")
+    public String VerifyPage() {
+        return "EmailVerify";  // loads register.html
+    }
+
 }
