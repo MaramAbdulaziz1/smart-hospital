512e178073fcedaa7373ec22265977c4f840888f
Liam Chen <tychen5555@gmail.com>
2025-08-12 22:10:30 +0100

    fix: profile.css adjust/ intake name adjust

--- src/main/java/com/team10/smarthospital/controllers/clinicalNotesController.java
+++ src/main/java/com/team10/smarthospital/controllers/clinicalNotesController.java
@@ -7,17 +7,17 @@ import org.springframework.ui.Model;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
-public class IntakeController {
+public class clinicalNotesController {
    //TODO: testing home page
    /*@GetMapping("/")
    public String home() {
       return "patientintake"; // to load homePage.html from templates folder
    }*/
 
-   @GetMapping("/patientintake")
+   @GetMapping("/clinicalNotes")
    public String IntakePage(Model model){
-      model.addAttribute("pageTitle", "Patient Intake");
-      return "patientintake";
+      model.addAttribute("pageTitle", "Clinical Notes");
+      return "clinicalNotes";
    }
 
 }
