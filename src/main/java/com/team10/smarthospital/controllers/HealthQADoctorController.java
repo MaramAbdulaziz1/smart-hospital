9be3c15a3d411db9a7d982bce91c073803dfe41d
Cindy626 <Cindy062689@gmail.com>
2025-08-16 17:54:16 +0100

    fix issue

--- src/main/java/com/team10/smarthospital/controllers/HealthQADoctorController.java
+++ src/main/java/com/team10/smarthospital/controllers/HealthQADoctorController.java
@@ -1,13 +1,15 @@
 package com.team10.smarthospital.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HealthQADoctorController {
 
   @GetMapping("/HealthQADoctor")
-  public String healthQADoctor() {
+  public String healthQADoctor(Model model) {
+    model.addAttribute("pageTitle", "Coming Soon");
     return "healthQADoctor";
   }
 }
