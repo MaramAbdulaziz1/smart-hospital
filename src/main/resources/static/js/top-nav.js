10c39528a41b9929b5c2beab53a023a2c47727eb
negi <nezii0331@gmail.com>
2025-08-23 20:59:52 +0800

    improve logout button

--- src/main/resources/static/js/top-nav.js
+++ src/main/resources/static/js/top-nav.js
@@ -53,30 +53,6 @@ function setupEventListeners() {
           });
       });
   }
-
-  // Logout functionality
-  const logoutBtns = document.querySelectorAll('[data-action="logout"]');
-  logoutBtns.forEach(btn => {
-      btn.addEventListener('click', () => {
-          if (confirm('Are you sure you want to log out?')) {
-              fetch('/user/logout', {
-                  method: 'POST',
-                  headers: {
-                      'Content-Type': 'application/json'
-                  }
-              })
-              .then(response => response.json())
-              .then(data => {
-                  if (data.code == '0')
-                  window.location.href = '/';
-              })
-              .catch(error => {
-                  console.error('Error:', error);
-                  alert('An error occurred during logout.');
-              });
-          }
-      });
-  });
 }
 
 async function fetchPatientProfile() {
